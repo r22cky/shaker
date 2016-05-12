6dfef633f1d0a9cd2030bf927fd7b9a4646a6ebc
Romain <romain.cky@gmail.com>
2016-05-12 12:46:18 +0200

    added routes and cocktails.index

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,2 +1,42 @@
 class CocktailsController < ApplicationController
+  before_action :set_coktail, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+    @cocktail = find_cocktail
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+    def set_cocktail
+      params.require(:cocktail).permit(:name)
+    end
+
+    def find_cocktail
+      @cocktail = Cocktail.find(params[:id])
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  root 'cocktails#index'
+  resources :cocktails do
+    resources :doses, only: [:new, :create]
+  end
+
+  resources :doses, only: [:destroy]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
