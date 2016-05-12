e5913130fd62b9b72bda44a718001fdf7274f1cb
Romain <romain.cky@gmail.com>
2016-05-12 18:11:21 +0200

    correction d'un bug sur la fonction destroy impliquant l'appel au params non necessaire

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,5 +1,5 @@
 class CocktailsController < ApplicationController
-  before_action :set_cocktail, only: [:edit, :update, :destroy]
+  before_action :set_cocktail, only: [:edit, :update]
 
   def index
     @cocktails = Cocktail.all
