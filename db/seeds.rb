94711b79dead2069e6f7ed6a8c563e10584ea555
Romain <romain.cky@gmail.com>
2016-05-13 17:10:01 +0200

    insertion formulaire commentaires

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+  Ingredient.destroy_all
   Ingredient.create(name: "absinthe")
   Ingredient.create(name: "bailays")
   Ingredient.create(name: "bitter")
